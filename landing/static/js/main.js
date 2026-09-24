d045d3299ea62601ac1ef0d83b3ac6337fa56a06
Tyler Applebaum <tyler@us175.com>
2026-09-24 10:04:20 -0400

    menu style updated to match the rest of the US175 sites

--- landing/static/js/main.js
+++ landing/static/js/main.js
@@ -1,46 +1,9 @@
-(function (window, document) {
-    var menu = document.getElementById('menu'),
-        rollback,
-        WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange' : 'resize';
+(function () {
+    var toggle = document.querySelector('.site-nav-toggle');
+    var menu = document.getElementById('site-nav-links');
 
-    function toggleHorizontal() {
-        menu.classList.remove('closing');
-        [].forEach.call(
-            document.getElementById('menu').querySelectorAll('.custom-can-transform'),
-            function (el) {
-                el.classList.toggle('pure-menu-horizontal');
-            }
-        );
-    };
-
-    function toggleMenu() {
-        // set timeout so that the panel has a chance to roll up
-        // before the menu switches states
-        if (menu.classList.contains('is-open')) {
-            menu.classList.add('is-closing');
-            rollBack = setTimeout(toggleHorizontal, 500);
-        }
-        else {
-            if (menu.classList.contains('closing')) {
-                clearTimeout(rollBack);
-            } else {
-                toggleHorizontal();
-            }
-        }
-        menu.classList.toggle('is-open');
-        document.getElementById('toggle').classList.toggle('x');
-    };
-
-    function closeMenu() {
-        if (menu.classList.contains('is-open')) {
-            toggleMenu();
-        }
-    }
-
-    document.getElementById('toggle').addEventListener('click', function (e) {
-        toggleMenu();
-        e.preventDefault();
+    toggle.addEventListener('click', function () {
+        var expanded = menu.classList.toggle('is-open');
+        toggle.setAttribute('aria-expanded', String(expanded));
     });
-
-    window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
-})(this, this.document);
+})();
